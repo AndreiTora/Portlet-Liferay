8747464c37fed4e4a84d974de6981ef4315d28f5
acanas <acanas@ACANAS10.indra.es>
2020-05-19 09:42:55 +0200

    creacion csv del layout

--- MiPrimerPortlet/modules/Portlet1/src/main/java/com/andrea/formacion/portlet1/portlet/Portlet1Portlet.java
+++ MiPrimerPortlet/modules/Portlet1/src/main/java/com/andrea/formacion/portlet1/portlet/Portlet1Portlet.java
@@ -1,22 +1,21 @@
 package com.andrea.formacion.portlet1.portlet;
 
 import com.andrea.formacion.portlet1.constants.Portlet1PortletKeys;
+import com.andrea.formacion.portlet1.utils.CSVUtils;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.io.OutputStreamWriter;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutService;
 import com.liferay.portal.kernel.util.WebKeys;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Vector;
 
 import javax.portlet.Portlet;
@@ -47,7 +46,7 @@ import org.osgi.service.component.annotations.Component;
 public class Portlet1Portlet extends MVCPortlet {
 	
 	LayoutService layoutService;
-	
+
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse)
 			throws IOException, PortletException {
@@ -69,9 +68,7 @@ public class Portlet1Portlet extends MVCPortlet {
 			lista.add(cliente2);
 			
 			renderRequest.setAttribute("listaClientes", lista);
-			
-			
-			//String CSV_SEPARATOR = ",";
+
 			
 			Layout layoutKey = (Layout) renderRequest.getAttribute(WebKeys.LAYOUT);
 			
@@ -79,10 +76,12 @@ public class Portlet1Portlet extends MVCPortlet {
 			try {
 				Layout layout = LayoutLocalServiceUtil.getLayout(layoutKey.getLayoutId());
 				
-				System.out.println(layout);
 				
-				//System.out.println(layout);
+				System.out.println(layout.getChildren());
 				
+				System.out.println("  ---  ");
+				
+				System.out.println(layout.getAncestors());
 				File file = new File("C:/Users/acanas/git/primerportletgit/MiPrimerPortlet/modules/Portlet1/src/main/resources/META-INF/resources/portlet.csv");
 				  
 				//Create the file
@@ -92,11 +91,29 @@ public class Portlet1Portlet extends MVCPortlet {
 				} else {
 				    System.out.println("File already exists.");
 				}
+				
+				String csvFile = "C:/Users/acanas/git/primerportletgit/MiPrimerPortlet/modules/Portlet1/src/main/resources/META-INF/resources/portlet.csv";
+				FileWriter writer = new FileWriter(csvFile);
 				 
-				//Write Content
-				FileWriter writer = new FileWriter(file);
-				writer.write(layout.toXmlString());
-				writer.close();
+				 List<Layout> layouts = Arrays.asList(layout);
+
+			        //for header
+			        CSVUtils.writeLine(writer, Arrays.asList("Title", "URL", "Type"));
+
+			        for (Layout d : layouts) {
+
+			            List<String> list = new ArrayList<>();
+			            
+			            list.add(d.getTitle());
+			            list.add(d.getFriendlyURL());
+			            list.add(d.getType());
+			            
+			            CSVUtils.writeLine(writer, list);
+
+			        }
+
+		        writer.flush();
+		        writer.close();
 			} catch (PortalException e) {
 				e.printStackTrace();
 			}
