9790c5b245192fe3358fd8abb5cf345f0adaab2a
acanas <acanas@ACANAS10.indra.es>
2020-07-20 11:59:44 +0200

    botón provisional para método + inputstream XML Reader

--- MiPrimerPortlet/modules/PortletXML/src/main/java/PortletXML/portlet/PortletXMLPortlet.java
+++ MiPrimerPortlet/modules/PortletXML/src/main/java/PortletXML/portlet/PortletXMLPortlet.java
@@ -3,15 +3,25 @@ package PortletXML.portlet;
 import PortletXML.constants.PortletXMLPortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.DocumentException;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
+import javax.portlet.ProcessAction;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.xml.soap.Node;
 
 import org.osgi.service.component.annotations.Component;
+import org.xml.sax.InputSource;
 
 /**
  * @author acanas
@@ -39,5 +49,23 @@ public class PortletXMLPortlet extends MVCPortlet {
 		super.render(renderRequest, renderResponse);
 		
 	}
+	
+	@ProcessAction(name = "readFile")
+	public void uploadFileAction(ActionRequest actionRequest, ActionResponse actionResponse) 
+			throws IOException, PortletException {
+		
+		System.out.println("hola");
+		try {
+			System.out.println("hol");
+			FileInputStream is = new FileInputStream("C:/Users/acanas/Documents/urlrewrite.xml");
+			Document doc = SAXReaderUtil.read(is);	
+			System.out.println(doc);
+			doc.toString();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+    }	
 	
 }
--- MiPrimerPortlet/modules/PortletXML/src/main/resources/META-INF/resources/view.jsp
+++ MiPrimerPortlet/modules/PortletXML/src/main/resources/META-INF/resources/view.jsp
@@ -1,5 +1,25 @@
 <%@ include file="/init.jsp" %>
 
 <p>
-	<b><liferay-ui:message key="portletxml.caption"/></b>
+	<%@ include file="/init.jsp" %>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %>
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+<%@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
+<%@ taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %>
+<liferay-theme:defineObjects/>
+<portlet:defineObjects/>
+<p>Lectura <b>Archivo XML</b> portlet.</p>
+ 
+<portlet:actionURL name="readFile" var="readFile">
+</portlet:actionURL>
+ 
+<form action="${readFile}" method="post" enctype="multipart/form-data">
+    
+    <p><aui:button type="submit" value="upload" name="upload" cssClass="topSpace"/></p>
+</form>
+
+<p><b><a href='<portlet:renderURL/>'>Volver</a></b></p>
+
 </p>
